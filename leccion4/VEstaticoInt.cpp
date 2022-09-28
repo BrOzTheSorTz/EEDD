0bc1170a02ff3746fc27c4f2dcad1443340d6550
Jose Pablo <sortzweb@gmail.com>
2022-09-28 13:14:27 +0200

    Creada la clase Vector estático int

--- leccion4/VEstaticoInt.cpp
+++ leccion4/VEstaticoInt.cpp
@@ -4,3 +4,66 @@
 //
 
 #include "VEstaticoInt.h"
+#include <algorithm>
+
+VEstaticoInt::VEstaticoInt(int tama) :_tam(tama){
+    _v = new int[tama];
+}
+
+VEstaticoInt::VEstaticoInt(const VEstaticoInt &orig) {
+    _tam = orig._tam;
+    _v = new int [_tam];
+    for(int i=0;i<_numElementos;++i){
+        _v[i] = orig._v[i]; //Necesario copiar los elementos por separado
+    }
+
+}
+
+VEstaticoInt &VEstaticoInt::operator=(const VEstaticoInt &orig) {
+    if(this != &orig){
+        delete [] _v;
+        _v = new int [orig._tam];
+        for(int i=0;i<orig._numElementos;++i){
+            _v[i] = orig._v[i];
+
+        }
+
+    }
+
+    return *this;
+}
+//De mayor a menor
+void VEstaticoInt::ordenar() {
+    std::sort(_v,_v + _numElementos);//El primer, ultimo elemento
+
+
+}
+
+int VEstaticoInt::busqueda(int dato) {
+    int inf = 0;
+    int sup = _numElementos -1;
+    int medio = (inf + sup)/2;
+
+    while(inf <= sup){
+        if(_v[medio] == dato){
+            return medio;
+        }else if(_v[medio] > dato){
+            sup = medio-1;
+
+        }else{
+            inf = medio +1;
+        }
+
+    }
+
+
+
+    return -1;
+}
+
+VEstaticoInt::~VEstaticoInt() {
+    delete [] _v;
+
+}
+
+
--- leccion4/VEstaticoInt.h
+++ leccion4/VEstaticoInt.h
@@ -8,6 +8,26 @@
 
 
 class VEstaticoInt {
+private:
+    int _tam;
+    int _numElementos;
+    int *_v = nullptr;
+
+public:
+    VEstaticoInt(int tama);
+    VEstaticoInt(const VEstaticoInt& orig);
+    VEstaticoInt& operator=(const VEstaticoInt& orig);
+
+    int lee(unsigned pos){return _v[pos];}
+    void escribe(unsigned pos,int dato){_v[pos] = dato;}
+    int &operator[](unsigned pos){
+        return _v[pos];
+    }
+
+    void ordenar();
+    int busqueda(int dato);
+
+    virtual ~VEstaticoInt();
 
 };
 
